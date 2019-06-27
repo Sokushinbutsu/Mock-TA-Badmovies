d3defb04af78e300d499222b6fce9698b788da4a
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-06-26 21:29:10 -0400

    Added db schema

--- db/sql/moviesSchema.sql
+++ db/sql/moviesSchema.sql
@@ -1,1 +1,9 @@
 -- SET UP SCHEMA HERE
+CREATE DATABASE IF NOT EXISTS `badmovies`;
+CREATE TABLE IF NOT EXISTS `badmovies`.`favorites` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `poster_path` VARCHAR(45) NULL,
+  `title` VARCHAR(45) NULL,
+  `release_date` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
